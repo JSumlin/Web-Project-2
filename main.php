dc70233b2954bd8e1c531da8b902cc0ab44c6480
tottinguyen1 <144629323+tottinguyen1@users.noreply.github.com>
2023-11-15 18:10:04 -0500

    Add files via upload

--- main.php
+++ main.php
@@ -8,23 +8,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hangman Game: Main </title>
+    <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-    <h1>Hangman Game</h1>
-    <p>Welcome, <?php echo $_SESSION["user_name"] ?></p>
+<h1> The Hangman Game </h1>
+    <div class="login">   
+        <h2>Welcome, <?php echo $_SESSION["user_name"] ?></h2>
 
-    <p>Pick a level:</p>
-    <form action="" method="post">
-        <input type="submit" name="easy" value="Easy">
-        <input type="submit" name="medium" value="Medium">
-        <input type="submit" name="hard" value="Hard">
-    </form>
-    <p> <a href="leaderboard"> Leader board</a></p>
+        <p>Pick a level </p>
+        <form action="" method="post">
+            <input type="submit" name="easy" value="Easy">
+            <input type="submit" name="medium" value="Medium">
+            <input type="submit" name="hard" value="Hard">
+
+            <p> <a href="leaderboard.php"> Leader board</a></p>
+        </form>
+    
+
+    </div>   
+
 </body>
 </html>
 
 <?php
-    if(isset($_POST["easy"])) {
+    if(isset($_POST["easy"])){
         $_SESSION["level"] = $_POST["easy"];
         header("location:game.php");
         exit;
@@ -41,3 +48,4 @@
         header("location:game.php");
         exit;
     }
+?>
